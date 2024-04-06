056a5507b391a3126555d6e968c981943d554b74
mobyitalong <165924835+Mobyitalong@users.noreply.github.com>
2024-04-06 11:20:55 +0000

    finish structs, methods * interfaces

--- src/app/structs_methods_interfaces/shapes_test.go
+++ src/app/structs_methods_interfaces/shapes_test.go
@@ -13,23 +13,40 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		var got float64 = shape.Area()
+		if got != want {
+			t.Errorf("Incorrect area returned: got %g want %g", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
+}
+
+// Table driven approach
+func TestAreaTableDrivenApproach(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Height: 12, Width: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		var got float64 = tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("Incorrect area returned: %#v got %g want %g", tt.shape, got, tt.want)
+		}
+	}
 }
